2ba24daa44de1cfb9ab480d8bb8363457ecaa3b9
AravindIM <mail.aravindim@proton.me>
2023-03-02 20:36:34 +0530

    Fix: Sway has default.nix and home.nix

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,24 +1,25 @@
-{ lib, inputs, system, home-manager, user, nix-doom-emacs,... }:
+{ lib, inputs, system, home-manager, user, doom-emacs,... }:
 
 {
   thinkpad = lib.nixosSystem {
     inherit system;
-    specialArgs = { inherit user inputs nix-doom-emacs;};
+    specialArgs = { inherit user inputs doom-emacs;};
     modules = [
       ./thinkpad/configuration.nix
 
       home-manager.nixosModules.home-manager {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = { inherit user nix-doom-emacs;};
+        home-manager.extraSpecialArgs = { inherit user doom-emacs;};
         home-manager.users.${user} = {
           imports = [
             ./thinkpad/home.nix
-            nix-doom-emacs.hmModule
+            ../modules/desktop/sway/home.nix
+            doom-emacs.hmModule
           ] ++ (import ../modules/shell)
           ++ (import ../modules/editor);
         };
       }
-    ];
+    ] ++ [(import ../modules/desktop/sway)];
   };
 }
